c764c96af97a192865038b373dcbd74f4094201a
KBonnicksen <kelseybon@gmail.com>
2019-07-22 17:50:46 -0700

    Created the addMovie controller and view

---
+++ MovieMVCModelBinding/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieMVCModelBinding.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+    }
+}
